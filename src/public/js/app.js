656d121e081551680a067b01b1c2dc8766ff7755
hyeone22 <seunghyeone2@gmail.com>
2025-01-28 16:27:05 +0900

    WebSocket Message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected Server✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message :", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected Server❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from Browser");
+}, 10000);
