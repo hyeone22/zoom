8a32de6057f4c83f53c7f898bcf4560bca03c201
hyeone22 <seunghyeone2@gmail.com>
2025-01-28 17:06:11 +0900

    WebSocket Each Message Browser

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import { getDefaultAutoSelectFamilyAttemptTimeout } from "net";
 import WebSocket from "ws";
 
 const app = express();
@@ -14,15 +15,18 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected from Browser✅");
   socket.on("close", () => {
     console.log("Disconnected from the Browser❌");
   });
   socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
+    console.log(message.toString("utf-8"));
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
   });
-  socket.send("hello");
 });
 
 server.listen(3000, handleListen);
