2f7760e34da18d3df7cc319e9e8933496bc01a07
hyeone22 <seunghyeone2@gmail.com>
2025-01-28 18:08:38 +0900

    WebSocket Setting Nickname

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import http from "http";
-import { getDefaultAutoSelectFamilyAttemptTimeout } from "net";
 import WebSocket from "ws";
 
 const app = express();
@@ -19,13 +18,22 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected from Browser✅");
   socket.on("close", () => {
     console.log("Disconnected from the Browser❌");
   });
-  socket.on("message", (message) => {
-    console.log(message.toString("utf-8"));
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
   });
 });
 
