fe3abe890f9983f44887ee7bf7303499d66ab0fb
hyeone22 <seunghyeone2@gmail.com>
2025-01-29 17:56:55 +0900

    Recap

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = io();
+
+const welcome = document.getElementById("Welcome");
+const form = welcome.querySelector("form");
+
+function backendDone(msg) {
+  console.log(`The Backend said :`, msg);
+}
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = document.querySelector("input");
+  socket.emit("enter_room", input.value, backendDone);
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
